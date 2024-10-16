7796b97e332002ad51b7fa632b0148b3ac4692ab
AkibRiad <akibriad652@gmail.com>
2024-10-16 21:59:25 +0600

    map in js

---
+++ essix.js
@@ -0,0 +1,19 @@
+// const data = [{id:5, name:'akib',address:'savar'}]
+
+// console.log(data[0].id);
+
+const products = {
+    count:5000,
+    data:{
+        nameee:{id:1,name:'akib',occupation:'student'}
+        
+    }
+
+}
+
+// products.data[0].id = 1123;
+// console.log(products)
+
+// console.log(products.data[1].occupa)
+
+console.log(products.data.nameee.name)
---
+++ maf.js
@@ -0,0 +1,21 @@
+// const friends = ['akib','israt','hamim','riad'];
+
+// const evenFriends =[];
+
+// for(const name of friends){
+//     if(name.length % 2 === 0){
+//         evenFriends.push(num);
+//     }
+
+
+// }
+// console.log(evenFriends)
+
+const numbers = [1,2,3,4];
+
+
+let sum = 0;
+for(const num of numbers){
+    sum = sum + (num*2);
+}
+console.log(sum)
